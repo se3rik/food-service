8df7b10bd8b5489b71c41acbccb602323abbc83b
se3rik <se3rik@mail.ru>
2025-09-29 02:03:59 +0300

    feat: active state for nav-links

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,9 +1,11 @@
-import { Link, Outlet } from 'react-router';
+import { NavLink, Outlet } from 'react-router';
+import cn from 'classnames';
 
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
 
 export function Layout() {
+
 	return (
 		<div className={styles['layout']}>
 			<div className={styles['sidebar']}>
@@ -17,14 +19,18 @@ export function Layout() {
 					</div>
 				</div>
 				<div className={styles['menu']}>
-					<Link to="/" className={styles['link']}>
+					<NavLink to="/" className={({ isActive }) => cn(styles['link'], {
+						[styles['active']]: isActive
+					})}>
 						<img src="/icons/MenuIcon.svg" alt="menu-icon" />
 						Menu
-					</Link>
-					<Link to="/cart" className={styles['link']}>
+					</NavLink>
+					<NavLink to="/cart" className={({ isActive }) => cn(styles['link'], {
+						[styles['active']]: isActive
+					})}>
 						<img src="/icons/CartIcon.svg" alt="cart-icon" />
 						Cart
-					</Link>
+					</NavLink>
 				</div>
 				<Button className={styles['leave-btn']}>
 					<img src="/icons/LeaveIcon.svg" alt="leave-button" />
