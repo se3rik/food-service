1ea3fd963d05fb9a1fe1c1c97cc6a2f17013d9ba
se3rik <se3rik@mail.ru>
2025-09-30 18:37:49 +0300

    feat: layout && stylization ProductCard component

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router';
+import styles from './ProductCard.module.css';
+
+import type { ProductCardProps } from './ProductCard.props';
+
+function ProductCard(props: ProductCardProps) {
+	return (
+		<Link to={'/'} className={styles['card-link']}>
+			<div className={styles['card']}>
+				<div className={styles['head']} style={{ backgroundImage: `url('${props.image}')` }}>
+					<div className={styles['price']}>
+						{props.price}&nbsp;
+						<span className={styles['currency']}>₽</span>
+					</div>
+					<button className={styles['add-to-cart']}>
+						<img src="/icons/AdditionalCardIcon.svg" alt="cart-button" />
+					</button>
+					<div className={styles['rating']}>
+						{props.rating}
+						<img src="/icons/RatingIcon.svg" alt="rating-icon" />
+					</div>
+				</div>
+
+				<div className={styles['footer']}>
+					<div className={styles['title']}>{props.title}</div>
+					<div className={styles['description']}>{props.description}</div>
+				</div>
+			</div >
+		</Link>
+	);
+}
+
+export default ProductCard;
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -2,12 +2,25 @@ import styles from './Menu.module.css';
 
 import Headling from '../../components/Headling/Headling';
 import Search from '../../components/Search/Search';
+import ProductCard from '../../components/ProductCard/ProductCard';
 
 export function Menu() {
 	return (
-		<div className={styles['head']}>
-			<Headling>Меню</Headling>
-			<Search placeholder='Введите блюдо или состав' />
-		</div>
+		<>
+			<div className={styles['head']}>
+				<Headling>Меню</Headling>
+				<Search placeholder='Введите блюдо или состав' />
+			</div>
+			<div>
+				<ProductCard
+					id={1}
+					title='Наслаждение'
+					description='Салями, руккола, помидоры, оливки'
+					image='/images/Pizza.png'
+					price={300}
+					rating={4.5}
+				/>
+			</div>
+		</>
 	);
 }
