5468da2f3f2e6222ac95b4aaafd639ba5b1d4b61
se3rik <se3rik@mail.ru>
2025-10-06 22:46:19 +0300

    feat: login form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,8 +1,37 @@
+import { Link } from 'react-router';
+
+import styles from './Login.module.css';
+
+import Button from '../../components/Button/Button';
+import Headling from '../../components/Headling/Headling';
+import Input from '../../components/Input/Input';
+import type { FormEvent } from 'react';
+
 function Login() {
+	const submit = (e: FormEvent) => {
+		e.preventDefault();
+		console.log(e);
+	};
+
 	return (
-		<>
-			Login Page
-		</>
+		<div className={styles['login']}>
+			<Headling>Вход</Headling>
+			<form className={styles['form']} onSubmit={submit}>
+				<div className={styles['field']}>
+					<label htmlFor='email'>Ваш email</label>
+					<Input id='email' placeholder='Email' />
+				</div>
+				<div className={styles['field']}>
+					<label htmlFor='password'>Ваш пароль</label>
+					<Input id='password' type='password' placeholder='Пароль' />
+				</div>
+				<Button appearence='big'>Вход</Button>
+			</form>
+			<div className={styles['links']}>
+				<div>Нет аккаунта?</div>
+				<Link to={'/auth/registration'}>Зарегистрироваться</Link>
+			</div>
+		</div>
 	);
 }
 
